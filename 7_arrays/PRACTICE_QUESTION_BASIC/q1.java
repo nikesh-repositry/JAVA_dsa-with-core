7ac0f42ae40b0fd64884800988f10e71267d7b35
NIKESH <nikeshnoida@gmail.com>
2026-01-28 20:24:00 +0530

    Add files via upload

---
+++ 7_arrays/PRACTICE_QUESTION_BASIC/q1.java
@@ -0,0 +1,19 @@
+package ARRAYS;
+import java.util.Scanner;
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i=0;i<arr.length;i++){
+            System.out.println("enter the number of index " + i);
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                System.out.println("negative number is "+arr[i]+" at index "+i);
+            }
+        }
+    }
+}
