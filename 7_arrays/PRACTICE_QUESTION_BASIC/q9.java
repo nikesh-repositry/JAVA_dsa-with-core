d0a7572d78d25f708a8cb91a255aee1f3586e08b
NIKESH <nikeshnoida@gmail.com>
2026-01-31 16:01:57 +0530

    Add files via upload

---
+++ 7_arrays/PRACTICE_QUESTION_BASIC/q9.java
@@ -0,0 +1,15 @@
+//EASY
+//REVERSE ARRAY
+package ARRAYS;
+import java.util.Arrays;
+public class q9 {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        for(int i=0;i< arr.length/2;i++){
+            int temp = arr[i];
+            arr[i]=arr[arr.length-1-i];
+            arr[arr.length-1-i]=temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
