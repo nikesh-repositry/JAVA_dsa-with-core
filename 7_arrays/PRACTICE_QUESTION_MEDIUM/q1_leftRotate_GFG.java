69b9a0836e34a4315374d311e52129da3b7eb181
NIKESH <nikeshnoida@gmail.com>
2026-02-26 23:19:50 +0530

    RENAMED AND OPTIMIZED CODE

--- 7_arrays/PRACTICE_QUESTION_MEDIUM/q1_leftRotate_GFG.java
+++ 7_arrays/PRACTICE_QUESTION_MEDIUM/q1_leftRotate_GFG.java
@@ -1,6 +1,9 @@
+/*MEDIUM
+LEFT ROTATE THE ARRAY BY K
+ */
 package ARRAYS;
 import java.util.Arrays;
-public class q10 {
+public class q1_leftRotate_GFG {
     static void reverse(int[] arr, int start,int end){
         while(start<end){
             int temp = arr[start];
@@ -13,14 +16,21 @@ public class q10 {
     static void rotate(int[] arr,int d){
         int n = arr.length;
         d=d%n;
+        System.out.println(d);
         reverse(arr,0,d-1);
         reverse(arr,d,n-1);
         reverse(arr,0,n-1);
     }
     public static void main(String[] args) {
         int[] arr ={1,2,3,4};
-        int d = 1;
+        int d = 2;
         rotate(arr,d);
         System.out.println(Arrays.toString(arr));
     }
 }
+
+/* THE TRICK IS
+REVERSE THE FIRST PART FROM START TO K
+REVERSE THE SECOND PART FROM K TO END
+REVERSE THE WHOLE ARRAY AT LAST
+ */
