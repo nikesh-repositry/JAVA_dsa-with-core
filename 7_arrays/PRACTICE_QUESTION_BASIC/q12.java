2f40536eebda7286ca2309a97e23563f426e210b
NIKESH <nikeshnoida@gmail.com>
2026-02-27 00:01:43 +0530

    Add files via upload

---
+++ 7_arrays/PRACTICE_QUESTION_BASIC/q12.java
@@ -0,0 +1,19 @@
+//EASY
+//FIND MISSING NUMBER
+package ARRAYS;
+public class q12 {
+    public static void main(String[] args) {
+        int[] arr = {3,1,0};
+        int n = arr.length;
+        int total_sum = n*(n+1)/2;
+        int array_sum =0;
+        for(int i :arr){
+            array_sum+=i;
+        }
+        System.out.println(total_sum-array_sum);
+    }
+}
+/*
+take out the expected sum then subtract it by actual array sum
+then the difference is the actual required number 
+ */
