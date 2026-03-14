d73187513af29316192755b9583663e1294f9834
NIKESH <nikeshnoida@gmail.com>
2026-03-14 16:45:47 +0530

    segregation logic using quick sort approach

--- 7_arrays/PRACTICE_QUESTION_BASIC/q18_segregrate0and1.java
+++ 7_arrays/PRACTICE_QUESTION_BASIC/q18_segregrate0and1.java
@@ -1,23 +1,23 @@
-//segrerate 0 and 1 but without counting 
-class Solution {
-    void segregate0and1(int[] arr) {
-        int low = 0;
-        int high = arr.length-1;
-        while(low<high){
-            if((arr[high]==0)&&(arr[low]==1)){
-                int temp = arr[high];
-                arr[high] = arr[low];
-                arr[low] = temp;
-                low++;
-                high--;
-            }
-            else if(arr[low]==0){
-                low++;
-            }
-            else{
-                high--;
+package ARRAYS;
+//using quick sort
+public class q15 {
+    public static void segregate(int[] arr) {
+        int a =0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                int temp = arr[i];
+                arr[i] =  arr[a];
+                arr[a] = temp;
+                a++;
             }
         }
-        
+    }
+    public static void main(String[] args){
+        int[] array = {0,1,0,1,0,0,1};
+        segregate(array);
+        for(int num:array){
+            System.out.print(num+" ");
+        }
     }
 }
+//taking a pivot then arranging all big in right and smaller to left of it
