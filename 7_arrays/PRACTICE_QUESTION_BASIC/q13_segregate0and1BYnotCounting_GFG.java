278822db5516ddded2f1b1727f3025766b4398fe
NIKESH <nikeshnoida@gmail.com>
2026-03-14 16:45:08 +0530

    Add segregation method for 0s and 1s in array
    Implemented a method to segregate 0s and 1s in an array without counting.

--- 7_arrays/PRACTICE_QUESTION_BASIC/q13_segregate0and1BYnotCounting_GFG.java
+++ 7_arrays/PRACTICE_QUESTION_BASIC/q13_segregate0and1BYnotCounting_GFG.java
@@ -1,1 +1,23 @@
-
+//segrerate 0 and 1 but without counting 
+class Solution {
+    void segregate0and1(int[] arr) {
+        int low = 0;
+        int high = arr.length-1;
+        while(low<high){
+            if((arr[high]==0)&&(arr[low]==1)){
+                int temp = arr[high];
+                arr[high] = arr[low];
+                arr[low] = temp;
+                low++;
+                high--;
+            }
+            else if(arr[low]==0){
+                low++;
+            }
+            else{
+                high--;
+            }
+        }
+        
+    }
+}
