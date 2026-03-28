cfc34bb9ec735701cba44ee6f1444ddbbfc62ecd
NIKESH <nikeshnoida@gmail.com>
2026-03-28 12:33:38 +0530

    Add explanation comment for removeDuplicates function
    Add a comment explaining the function's input and output.

---
+++ 7_arrays/PRACTICE_QUESTION_BASIC/q24_removeDuplicate_LEETCODE.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0) return 0;
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+        
+    }
+}
+/*
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).*/
