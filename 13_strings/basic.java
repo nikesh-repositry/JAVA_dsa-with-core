33e8b9db61825b973e27c2485e387974353c28ed
NIKESH <nikeshnoida@gmail.com>
2026-03-30 18:40:00 +0530

    Update basic.java

--- 13_strings/basic.java
+++ 13_strings/basic.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 public class basic {
     public static void main(String[] args) {
+        //strings are immutable, new object created on modification
         //in java a string is not a primitive data type like int or char. it is a class representing a sequence of characters
         //non primitive behaviour = because it is an object , a string variable stores a reference to the memory location where the character data is held
         //java internally maintains strings as character array char[]
@@ -35,5 +36,23 @@ public class basic {
         System.out.println(str.startsWith("nik")); //ture
         System.out.println(str.endsWith("kk")); //true
         System.out.println(str.substring(1,5)); //ikes
+
+        //"+" operator
+        //when you add a number to a string, the number is converted to a string and appended
+        //left to right execution
+        System.out.println("abc" + 10 + 20); // abc1020
+        System.out.println(10 + 20 + "abc"); // 30abc
+
+        //comparing string
+        //==(reference comparison) checks if both variables point to the same memory location. it may return false even if the text is identical if one is in the SCP and the other is in the heap
+        //.equals()(content comparison) checks if the actual characters are the same. this is the standard way to compare strings for equality
+        //compareTo(String other) compares strings lexicographically(alphabatically) it returns the difference in ASCII values of the first mismatched character
+        String a = "Java";
+        String b = new String("Java");
+        System.out.println(a == b);        // false (different references)
+        System.out.println(a.equals(b));   // true (same content)
+        System.out.println(a.compareTo("Jb")); // -1 (lexicographic)
+
+
     }
 }
