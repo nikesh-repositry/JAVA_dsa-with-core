00aacfac0fb34726afd1d4e1d510203cd433cd4d
NIKESH <nikeshnoida@gmail.com>
2026-04-02 14:02:00 +0530

    Change string value and fix substring loop condition

--- 13_strings/problems/005_printAllSubstring.java
+++ 13_strings/problems/005_printAllSubstring.java
@@ -2,19 +2,12 @@ package STRINGS;
 
 public class q3 {
     public static void main(String[] args) {
-        String str = "abcde0";
+        String str = "9845";
         for(int i=0;i<str.length();i++){
-            for(int j=i+1;j<str.length();j++){
+            for(int j=i+1;j<=str.length();j++){
                 System.out.print(str.substring(i,j)+" ");
             }
             System.out.println();
         }
     }
 }
-/*
-a ab abc abcd abcde 
-b bc bcd bcde 
-c cd cde 
-d de 
-e 
-*/
