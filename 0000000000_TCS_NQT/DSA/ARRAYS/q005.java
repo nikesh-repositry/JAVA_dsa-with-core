31cbf246500891d606d7602a64cda056cdc9ab1e
NIKESH <nikeshnoida@gmail.com>
2026-05-24 01:10:36 +0530

    Implement threeSumClosest method for LeetCode 16

--- 0000000000_TCS_NQT/DSA/ARRAYS/q005.java
+++ 0000000000_TCS_NQT/DSA/ARRAYS/q005.java
@@ -1,1 +1,29 @@
-
+// implement 3sum closest leetcode 16
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        if(nums.length<3||nums.length==0){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int result = nums[0]+nums[1]+nums[2];
+        for(int i=0;i<nums.length-2;i++){
+            int j = i+1;
+            int k = nums.length-1;
+            while(k>j){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==target){
+                    return sum;
+                }    
+                if (Math.abs(sum - target) < Math.abs(result - target)) {
+                    result = sum;
+                }
+                if (sum < target) {
+                    j++;  
+                } else {
+                    k--;  
+                }        
+            }
+        }
+        return result;
+    }
+}
