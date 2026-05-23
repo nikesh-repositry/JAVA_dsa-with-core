dc096c85dee37823bdf3937afe40f24051d3dc6e
NIKESH <nikeshnoida@gmail.com>
2026-05-23 23:29:27 +0530

    Implement twoSum function for LeetCode problem

--- 0000000000_TCS_NQT/DSA/ARRAYS/q001.java
+++ 0000000000_TCS_NQT/DSA/ARRAYS/q001.java
@@ -1,1 +1,19 @@
-
+// two sum leetcode 1
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int i=0,j=nums.length-1;
+        while(i<j){
+            int sum = nums[i]+nums[j];
+            if(sum==target){
+                return new int[]{i,j};
+            }
+            else if(sum>target){
+                j--;
+            }else{
+                i++;
+            }
+        }return new int[]{};
+        
+    }
+}
