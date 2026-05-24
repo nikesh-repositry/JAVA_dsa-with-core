6bfe1e4c8277433bdfe33b68395e92ce0d55c042
NIKESH <nikeshnoida@gmail.com>
2026-05-24 16:50:33 +0530

    implement move 0 1 2 dutch national flag

--- 0000000000_TCS_NQT/DSA/ARRAYS/q007.java
+++ 0000000000_TCS_NQT/DSA/ARRAYS/q007.java
@@ -1,1 +1,31 @@
-
+// implement move 0 1 2 dutch national flag
+class Solution {
+    public void sort012(int[] arr) {
+        // code here
+        int i=0;
+        int j=0;
+        int k = arr.length-1;
+        while(j<=k){
+            switch (arr[j]){
+                case 0:
+                    int temp0 = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp0;
+                    i++;
+                    j++;
+                    break;
+                case 1:
+                    j++;
+                  
+                    break;
+                case 2:
+                    int temp1 = arr[k];
+                    arr[k] = arr[j];
+                    arr[j] = temp1;
+        
+                    k--;
+                    break;
+            }
+        }
+    }
+}
