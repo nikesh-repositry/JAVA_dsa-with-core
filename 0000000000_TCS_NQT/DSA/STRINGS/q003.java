c228addd2fa8048fd96f85d32f1ccfc89cc24bff
NIKESH <nikeshnoida@gmail.com>
2026-05-30 15:42:18 +0530

    Create q003.java

---
+++ 0000000000_TCS_NQT/DSA/STRINGS/q003.java
@@ -0,0 +1,16 @@
+package STRINGS;
+//find ascii code of char
+public class q3 {
+    public static void main(String[] args) {
+        char c = 'e';
+        int as = (int)c;
+        System.out.println(as);
+
+        String s = "hellow234";
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            int asi = (int)ch;
+            System.out.println(asi);
+        }
+    }
+}
