4e75d58a34355e197921d4f26ccc98a86a16ff68
NIKESH <nikeshnoida@gmail.com>
2026-05-30 17:16:56 +0530

    strong number or not

---
+++ 0000000000_TCS_NQT/DSA/NUMBERS/q014.java
@@ -0,0 +1,38 @@
+//strong number
+package NUMBERS;
+
+import java.util.Scanner;
+
+public class StrongNumberEasy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input number
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int temp = n;
+        int sum = 0;
+
+        // Check each digit
+        while (temp > 0) {
+            int digit = temp % 10;
+
+            // factorial of digit (inline, no method)
+            int fact = 1;
+            for (int i = 1; i <= digit; i++) {
+                fact *= i;
+            }
+
+            sum += fact;
+            temp /= 10;
+        }
+
+        // Compare with original number
+        if (sum == n) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
